0781e2c073b13900f5393450bf98419438fb166d
mayowa ahmed <kennyasimi@gmail.com>
2024-09-30 19:42:03 +0100

    python operators

---
+++ python course/Introduction to programming/data types/mayowa.py
@@ -0,0 +1,10 @@
+num1 = 45
+num2 = 3
+print("number1:" , num1)
+print("number2:" , num2)
+print("Addition:" , num1+num2)
+print("Difference:" , num1-num2)
+print("Product:" , num1*num2)
+print("Division:" , num1/num2)
+Result = num1+num2
+print("result of given equation is:" , result)
