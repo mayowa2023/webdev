510cf6af727e28a1d8e25dcb1b36e6ee8bfbe2cf
mayowa ahmed <kennyasimi@gmail.com>
2024-11-04 18:38:31 +0100

    object

---
+++ python course/Introduction to programming/mayowa.py
@@ -0,0 +1,8 @@
+class student:
+    def_init_(self,grade,name):
+        self.grade=grade
+        self.name=name
+s1= student("10","Faith")
+print("the student name is", s1.name)
+print("the student is in grade", s1.grade)
+    
